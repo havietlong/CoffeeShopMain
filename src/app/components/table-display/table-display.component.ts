8934dc55ef9012d3e5b4f223708655c86998aaac
Ha Viet Long <havietlong.work@gmail.com>
2024-06-26 16:59:07 +0700

    added reciept's action and delete products/employees

--- src/app/components/table-display/table-display.component.ts
+++ src/app/components/table-display/table-display.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, OnChanges, Output} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../../services/products/products.service';
+import { EmployeeService } from '../../services/employee/employee.service';
 
 @Component({
   selector: 'app-table-display',
@@ -12,7 +15,35 @@ export class TableDisplayComponent implements OnChanges{
   @Output() notifyGetCatById: EventEmitter<number> = new EventEmitter<number>();
   @Input() columns: string[] = [];
   @Input() data: any = [];
+  itemId!:number;
   
+  currentPath: string = '';
+
+  constructor(private activatedRoute: ActivatedRoute,private productsService:ProductsService, private employeeService:EmployeeService) {        
+    // Subscribe to route changes to get the current path
+    this.activatedRoute.url.subscribe(url => {
+      this.currentPath = url.map(segment => segment.path).join('/');
+    });
+  }
+
+  deleteItem(id: any ) {
+    if (this.currentPath.includes('employees')) {
+      this.deleteEmployee(id);
+    } else if (this.currentPath.includes('products')) {
+      this.deleteProduct(id);
+    }
+  }
+
+  deleteEmployee(id: string) {
+    // Implement the logic to delete an employee
+    this.employeeService.deleteEmployee(id).subscribe();
+  }
+
+  deleteProduct(id: number) {
+    // Implement the logic to delete a product
+    this.productsService.deleteProduct(id).subscribe();
+  }
+
 
   ngOnChanges(): void {
     if (this.data && this.data.length > 0) {
